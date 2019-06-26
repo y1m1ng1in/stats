fe46e5a2af063118b384056d46769b0be27e853c
Yiming Lin <yl6@pdx.edu>
2019-06-26 13:23:05 -0700

    copyright updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Stats
-Copyright (c) 2019 Bart Massey
+Copyright (c) 2019 Yiming Lin
 
 This program computes a basic statistic. Its input is
 taken from `stdin`, and must consist of floating-point
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Copyright © 2019 Bart Massey
+// Copyright © 2019 Yiming Lin
 // [This program is licensed under the "MIT License"]
 // Please see the file LICENSE in the source
 // distribution of this software for license terms.
